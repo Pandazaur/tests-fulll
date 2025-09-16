b285dad89545a8b053c99827e169f1960c695bd2
Alexandre BOREL <alexandre.borel@paymium.com>
2025-09-16 15:40:54 +0200

    feat: checkbox management

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,43 @@
 import styles from './App.module.css'
 import IconDuplicate from "./components/ui/icons/IconDuplicate.tsx";
 import IconTrash from "./components/ui/icons/IconTrash.tsx";
-import CardUser from "./components/user/CardUser.tsx";
-import {useEffect, useState} from "react";
+import CardUser, { type User as CardUserType } from "./components/user/CardUser.tsx";
+import {type ChangeEvent, useEffect, useRef, useState} from "react";
 import useDebouncedValue from "./hooks/useDebouncedValue.ts";
 import GithubApi from "./services/api/Github.api.ts";
-import type {GitHubSearchUsersResponse} from "./types/response/github-api/SearchProfilesResponse.type.ts";
+import type { GitHubSearchUsersResponse } from "./types/response/github-api/SearchProfilesResponse.type.ts";
 
 function App() {
+    const checkAllCheckbox = useRef<HTMLInputElement>(null)
     const [search, setSearch] = useState('panda')
+    const [selectedUserIds, setSelectedUserIds] = useState<number[]>([])
     const [searchResult, setSearchResult] = useState<GitHubSearchUsersResponse | null>(null)
     const delayedSearch = useDebouncedValue(search, 500)
 
     useEffect(() => {
+        setSelectedUserIds([])
+
         if (delayedSearch.length) {
             searchUsers(delayedSearch)
         } else {
             setSearchResult(null)
         }
     }, [delayedSearch]);
+
+    useEffect(() => {
+        if (!checkAllCheckbox.current) {
+            return
+        }
+
+        if (!searchResult) {
+            checkAllCheckbox.current.indeterminate = false
+            checkAllCheckbox.current.checked = false
+            return
+        }
+
+        checkAllCheckbox.current.checked = selectedUserIds.length > 0 && selectedUserIds.length === searchResult.items.length
+        checkAllCheckbox.current.indeterminate = selectedUserIds.length > 0 && selectedUserIds.length !== searchResult.items.length
+    }, [selectedUserIds]);
 
     const searchUsers = async (search: string) => {
         try {
@@ -27,6 +46,29 @@ function App() {
         } catch (e) {
             console.error(e)
         }
+    }
+
+    const onToggleUser = (user: CardUserType, isNowSelected: boolean) => {
+        if (isNowSelected) {
+            setSelectedUserIds(previousSelectedUserIds => [...previousSelectedUserIds, user.id])
+        } else {
+            setSelectedUserIds(previousSelectedUserIds => previousSelectedUserIds.filter(id => id !== user.id))
+        }
+    }
+
+    const onToggleAllUsers = (e: ChangeEvent<HTMLInputElement>) => {
+        if (!searchResult?.items) {
+            return null
+        }
+
+        if (e.target.checked) {
+            setSelectedUserIds(
+                searchResult.items.map(user => user.id)
+            )
+        } else {
+            setSelectedUserIds([])
+        }
+
     }
 
     return (
@@ -49,8 +91,8 @@ function App() {
 
                 <div className={styles.actions}>
                     <div>
-                        <input type="checkbox"/>
-                        <label>X elements selected</label>
+                        <input type="checkbox" ref={checkAllCheckbox} onChange={onToggleAllUsers}/>
+                        <label>{selectedUserIds.length} elements selected</label>
                     </div>
 
                     <div className={styles.actions__buttons}>
@@ -65,7 +107,9 @@ function App() {
                 <div className={styles.userGrid}>
                     {
                         searchResult?.items.length
-                            ? searchResult?.items.map(user => <CardUser user={user}/>)
+                            ? searchResult?.items.map(user =>
+                                <CardUser user={user} isSelectable isSelected={selectedUserIds.includes(user.id)} onToggleSelect={onToggleUser}/>
+                            )
                             : null
                     }
                 </div>
--- frontend/src/components/user/CardUser.tsx
+++ frontend/src/components/user/CardUser.tsx
@@ -1,15 +1,36 @@
 import styles from './CardUser.module.css'
 import type {GitHubUser} from "../../types/response/github-api/SearchProfilesResponse.type.ts";
+// import {useEffect, useState} from "react";
+
+export type User = Pick<GitHubUser, 'id' | 'login' | 'avatar_url' | 'html_url'>
 
 type Props = {
+    user: User
     isSelectable?: boolean
-    user: Pick<GitHubUser, 'id' | 'login' | 'avatar_url' | 'html_url'>
+    isSelected?: boolean
+    onToggleSelect?: (user: User, isSelected: boolean) => unknown
 }
 
 export default function CardUser(props: Props) {
+    // const [isSelected, setSelected] = useState(props.isSelected ?? false)
+
+    // useEffect(() => {
+    //     props.onToggleSelect?.(props.user, isSelected)
+    // }, [isSelected]);
+
     return (
         <div className={styles.card}>
             <div className={styles.card__body}>
+                {
+                    props.isSelectable ? (
+                        <input
+                            className={styles.card__checkbox}
+                            type="checkbox"
+                            onChange={e => props.onToggleSelect?.(props.user, e.target.checked)}
+                            checked={props.isSelected}
+                        />
+                    ) : null
+                }
                 <img className={styles.card__avatar} src={props.user.avatar_url} alt="Avatar"/>
                 <p className={styles.card__text}>
                     <span className={styles.card__userId}>{props.user.id}</span>
